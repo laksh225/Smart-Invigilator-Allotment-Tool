010eaa9d03f2009efc9438b080a9e2e44d74e688
Laksh O <olaksh@gmail.com>
2021-07-25 18:24:06 +0530

    Add allotment logic and increment function
    Add allotment logic for DySP and Squad

--- faculty.py
+++ faculty.py
@@ -4,9 +4,12 @@ class Faculty:
         self.index = index
         self.flag = flag
         self.designation = designation
-    def __str__(self):
-        return f'{self.name} - {self.designation}'
-       
+    def __repr__(self):
+        return f'{self.name} - {self.designation} - {self.index} - {self.flag}'
+def increment_flag(fac_list):
+    for fac in fac_list:
+        fac.flag += 1
+   
 if __name__=='__main__':
     Person1 = Faculty(1, "Fac1", 0, "Professor")
     print(Person1)
--- main.py
+++ main.py
@@ -39,4 +39,39 @@ Level 3
 Flag
 •    Random selection of AP, P, ASSpof
 •    If duty then duty=duty+1
+
+Range 1-5 Professors
+6-14 Associate
+15-48 Assistant
 """
+
+from faculty import increment_flag
+from input import load
+import random
+
+fac_list = load()
+#print(fac_list)
+#days = input("Enter of days")
+#shifts =  input("Enter no. of shifts")
+DySp = []
+for i in range(1):
+    index = random.randint(1-1, 5-1)
+    #print([fac for fac in fac_list if fac.index==index])
+    DySp.append(fac_list[index])
+increment_flag(DySp)
+#print(set(DySp))
+
+def random_unique(start, stop, rep):
+    unique_list = []
+    while(True):
+        index = random.randrange(start, stop)
+        if index not in unique_list:
+            unique_list.append(index)
+            if len(unique_list) == rep:
+                break
+    return unique_list
+
+squad = random_unique(15-1, 48-1, 4)
+increment_flag([fac_list[fac] for fac in squad])
+print([fac_list[fac] for fac in squad])
+
