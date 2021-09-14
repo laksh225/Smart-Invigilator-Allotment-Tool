0271272dfff0ac221af90bc39bf36db59a4c7b73
karthikeya-rs <90641778+karthikeya-rs@users.noreply.github.com>
2021-09-14 21:42:03 +0530

    updated i/p file name

--- input.py
+++ input.py
@@ -6,7 +6,7 @@ from sys import argv
 import sqlite3
 
 def load():
-    con = sqlite3.connect('secdb.sqlite')
+    con = sqlite3.connect('inputdb.sqlite')
     cur = con.cursor()
     f_list = []
     p_list = []
@@ -17,16 +17,26 @@ def load():
     n = int(cur.fetchone()[0])
     
     for i in range(1, n+1): #i -> row
-            cur.execute('''SELECT * FROM ece WHERE ROWID=?''',(i,))
-        r=cur.fetchone()
-        #if(sheet1.cell(row=i, column=1).value != None):
-            #print(str(sheet1.cell(row=i, column=2).value))
-        if(r[2] =='Professor'):
-            p_list.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
-        elif(r[2]=='Associate Professor'): 
-            asop_list.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
-        elif(r[2]=='Assistant Professor'):
-            assp_list.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
+            cur.execute('''SELECT * FROM ece WHERE ROWID=?''',(i,))
+
+        r=cur.fetchone()
+
+        #if(sheet1.cell(row=i, column=1).value != None):
+
+            #print(str(sheet1.cell(row=i, column=2).value))
+
+        if(r[2] =='Professor'):
+
+            p_list.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
+
+        elif(r[2]=='Associate Professor'): 
+
+            asop_list.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
+
+        elif(r[2]=='Assistant Professor'):
+
+            assp_list.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
+
         else:
             others.append(Faculty(r[0], r[5], r[2],r[3],r[4]))
     f_list = [p_list]+[asop_list]+[assp_list]
